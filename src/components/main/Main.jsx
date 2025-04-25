bd5495224401ec4fc8679c6acd6192b2431f2b0f
ibrohim0725 <baxtiyorovibrohim513@gmail.com>
2025-04-25 23:14:24 +0500

    started

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,16 +1,22 @@
-import Product from "../product/Product"
-import "./Main.css"
+import Product from "../product/Product";
+import "./Main.css";
 
 export default function Main() {
-  const{title,description,isNew,img} ={
-    title:"XX99 Mark II Headphones",
-    description: "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
-    isNew:true,
-    img:"https://picsum.photos/200/300?grayscale"
-  }
+  const { title, description, isNew, desktop } = {
+    title: "XX99 Mark II Headphones",
+    description:
+      "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+    isNew: true,
+    desktop: "../assets/shared/desktop/image-xx99-mark-two-headphones.jpg",
+  };
   return (
     <main className="main">
-      <Product title={title} description={description} isNew={isNew} img={img}/>
+      <Product
+        title={title}
+        description={description}
+        isNew={isNew}
+        img={desktop}
+      />
     </main>
-  )
+  );
 }
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,13 +1,13 @@
 import Button from "../button/Button";
 import "./Product.css";
 
-export default function Product({ title, description, isNew, img }) {
+export default function Product({ title, description, isNew,img:{desktop}}) {
   return (
     <div>
       <picture>
         {/* <source media="(max-didth:375px)" srcSet={tablet} />
         <source media="(max-width:768px)" srcSet={mobile} /> */}
-        <img src={img} alt={title} width={540} height={560} />
+        <img src={desktop} alt={title} width={540} height={560} />
       </picture>
       <div>
         {isNew && <span>NEW PRODUCT</span>}
