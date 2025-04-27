2f038ff9db9f3457b0740d4e2fe610e918ff8161
ibrohim0725 <baxtiyorovibrohim513@gmail.com>
2025-04-27 12:45:00 +0500

    start

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,21 +1,20 @@
 import Product from "../product/Product";
 import "./Main.css";
+import  desktop from "../../assets/shared/desktop/image-xx99-mark-two-headphones.jpg"
+console.log(desktop);
 
 export default function Main() {
-  const { title, description, isNew, desktop } = {
+  const productItems = {
     title: "XX99 Mark II Headphones",
     description:
       "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
     isNew: true,
-    desktop: "../assets/shared/desktop/image-xx99-mark-two-headphones.jpg",
+    img:desktop
   };
   return (
     <main className="main">
       <Product
-        title={title}
-        description={description}
-        isNew={isNew}
-        img={desktop}
+        {...productItems}
       />
     </main>
   );
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,13 +1,13 @@
 import Button from "../button/Button";
 import "./Product.css";
 
-export default function Product({ title, description, isNew,img:{desktop}}) {
+export default function Product({ title, description, isNew,img}) {
   return (
     <div>
       <picture>
         {/* <source media="(max-didth:375px)" srcSet={tablet} />
         <source media="(max-width:768px)" srcSet={mobile} /> */}
-        <img src={desktop} alt={title} width={540} height={560} />
+        <img src={img.desktop} alt={title} width={540} height={560} />
       </picture>
       <div>
         {isNew && <span>NEW PRODUCT</span>}
